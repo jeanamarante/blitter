c4e550760becc524b2506cfafb11a9fa1d6a961a
Jean Amarante <jeanamaranteopensource@gmail.com>
2019-04-15 00:28:55 -0400

    {Update: *} Project restructure.

--- tasks/blitter.js
+++ tasks/blitter.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
-const mime = require('mime');
 const isImage = require('is-image');
 const readDir = require('recursive-readdir');
-const DataURI = require('datauri');
 
 // This task relies on async functionality.
 let asyncDone = null;
@@ -24,7 +21,7 @@ function testOriginalSrc (grunt, src) {
         if (typeof item !== 'string') {
             throwAsyncError(new Error('src must be string.'));
         } else if (!grunt.file.isDir(path.resolve(item))) {
-            throwAsyncError(new Error(item + ' must be directory in src.'));
+            throwAsyncError(new Error(`${item} must be directory in src.`));
         }
     }
 }
@@ -70,93 +67,20 @@ function throwAsyncError (err) {
  */
 
 function recurseSrcDirectories (grunt, fileData, options) {
-    let src = fileData.orig.src.map((item) => path.resolve(item));
     let promises = [];
 
     // Ignore everything except directories and image files.
     // Store function in array for recursive-readdir module.
     let ignoreCallback = [(file, stats) => !(stats.isDirectory() || isImage(file))];
 
-    for (let i = 0, max = src.length; i < max; i++) {
-        promises.push(readDir(src[i], ignoreCallback).then((value) => value, (err) => err));
+    for (let i = 0, max = fileData.src.length; i < max; i++) {
+        promises.push(readDir(fileData.src[i], ignoreCallback).then((value) => value, (err) => err));
     }
 
     Promise.all(promises)
         .then((values) => {
-            streamToDest(grunt, fileData, options, values.flat());
+            require('./encode/base64')(grunt, fileData, options, asyncDone, throwAsyncError, values.flat());
         }, throwAsyncError);
-}
-
-/**
- * Stream data URIs from matched image files into dest file with their
- * respective id and MIME.
- *
- * @function streamToDest
- * @param {Object} grunt
- * @param {Object} fileData
- * @param {Object} options
- * @param {Array} matches
- * @api private
- */
-
-function streamToDest (grunt, fileData, options, matches) {
-    // Always prepend useObjectURLs invocation before parseBuffer invocation.
-    let data = options.useObjectURLs ? 'BLITTER.useObjectURLs();' : '';
-
-    // Start parseBuffer wrap.
-    data += 'BLITTER.parseBuffer([';
-
-    let writable = fs.createWriteStream(path.resolve(fileData.dest));
-
-    writable.on('error', throwAsyncError);
-
-    writable.write(data, 'utf8', () => {
-        streamBuffer(grunt, matches, writable);
-    });
-}
-
-/**
- * Stream the buffer recursively inside the parseBuffer invocation.
- *
- * @function streamBuffer
- * @param {Object} grunt
- * @param {Array} matches
- * @param {WriteStream} writable
- * @api private
- */
-
-function streamBuffer (grunt, matches, writable) {
-    let match = matches.pop();
-
-    if (match === undefined) {
-        // End parseBuffer wrap.
-        writable.end(']);', 'utf8', () => {
-            asyncDone(true);
-        });
-    } else {
-        // Start DataURI wrap.
-        let data = '"' + path.parse(match).name + '","' + mime.getType(match) + '","';
-
-        writable.write(data, 'utf8', () => {
-            let data = new DataURI();
-
-            data.on('error', throwAsyncError);
-
-            // Everytime readable stream finishes piping all data to the
-            // writable stream move on to next matched file.
-            data.on('end', () => {
-                // End DataURI wrap.
-                let data = matches.length === 0 ? '"' : '",';
-
-                writable.write(data, 'utf8', () => {
-                    streamBuffer(grunt, matches, writable);
-                });
-            });
-
-            data.pipe(writable, { end: false });
-            data.encode(match);
-        });
-    }
 }
 
 module.exports = function (grunt) {
@@ -173,6 +97,14 @@ module.exports = function (grunt) {
 
             testOriginalSrc(grunt, fileData.orig.src);
             testDest(grunt, fileData.dest);
+
+            // Replace fileData with new object containing resolved paths that
+            // are commonly used throughout all modules.
+            fileData = {
+                src: fileData.orig.src.map((item) => path.resolve(item)),
+                dest: path.resolve(fileData.dest)
+            };
+
             recurseSrcDirectories(grunt, fileData, options);
         }
     });
---
+++ tasks/encode/base64.js
@@ -0,0 +1,155 @@
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const {Base64Encode} = require('base64-stream');
+
+let asyncDone = null;
+let throwAsyncError = null;
+
+// Array of matched files.
+let files = null;
+
+// WritableStream for dest file.
+let writable = null;
+
+// TransformStream that encodes buffer data to base64 data.
+let transform = null;
+
+let readableClosed = false;
+let transformClosed = false;
+
+/**
+ * Stream data URIs from matched image files into dest file with their
+ * respective id and MIME.
+ *
+ * @function initializeStreamToDest
+ * @param {Object} grunt
+ * @param {Object} fileData
+ * @param {Object} options
+ * @api private
+ */
+
+function initializeStreamToDest (grunt, fileData, options) {
+    // Always prepend useObjectURLs invocation before parseBuffer invocation.
+    let content = options.useObjectURLs ? 'BLITTER.useObjectURLs();' : '';
+
+    // Start parseBuffer wrap.
+    content += 'BLITTER.parseBuffer([';
+
+    writable = fs.createWriteStream(fileData.dest);
+
+    writable.once('error', throwAsyncError);
+
+    writable.write(content, 'utf8', streamMatch);
+}
+
+/**
+ * Stream buffer recursively in the parseBuffer invocation.
+ *
+ * @function streamMatch
+ * @api private
+ */
+
+function streamMatch () {
+    let match = files.pop();
+
+    if (match === undefined) {
+        // End parseBuffer wrap.
+        writable.end(']);', 'utf8', () => {
+            asyncDone(true);
+        });
+    } else {
+        let mimeType = mime.getType(match);
+        let wrapStart = `"${path.parse(match).name}","${mimeType}","`;
+
+        writable.write(wrapStart, 'utf8', streamEncodedData(match, mimeType));
+    }
+}
+
+/**
+ * @function streamEncodedData
+ * @param {String} file
+ * @param {String} mimeType
+ * @api private
+ */
+
+function streamEncodedData (file, mimeType) {
+    let readable = fs.createReadStream(file);
+
+    transform = new Base64Encode({ prefix: `data:${mimeType};base64,` });
+
+    readable.once('error', throwAsyncError);
+    readable.once('close', onReadableClose);
+
+    transform.once('end', onTransformEnd);
+    transform.once('error', throwAsyncError);
+    transform.once('close', onTransformClose);
+
+    readable.pipe(transform).pipe(writable, { end: false });
+}
+
+/**
+ * @function onReadableClose
+ * @api private
+ */
+
+function onReadableClose () {
+    readableClosed = true;
+
+    closeStream();
+}
+
+/**
+ * Manually destroy transform stream once readable stream is
+ * done reading data.
+ *
+ * @function onTransformEnd
+ * @api private
+ */
+
+function onTransformEnd () {
+    let stream = transform;
+
+    transform = null;
+
+    stream.destroy();
+}
+
+/**
+ * @function onTransformClose
+ * @api private
+ */
+
+function onTransformClose () {
+    transformClosed = true;
+
+    closeStream();
+}
+
+/**
+ * Once the readable and transform stream are closed move on to
+ * the next matched file.
+ *
+ * @function closeStream
+ * @api private
+ */
+
+function closeStream () {
+    if (!readableClosed || !transformClosed) { return undefined; }
+
+    readableClosed = false;
+    transformClosed = false;
+
+    let wrapEnd = files.length === 0 ? '"' : '",';
+
+    writable.write(wrapEnd, 'utf8', streamMatch);
+}
+
+module.exports = function (grunt, fileData, options, doneCallback, errorCallback, matches) {
+    asyncDone = doneCallback;
+    throwAsyncError = errorCallback;
+
+    files = matches;
+
+    initializeStreamToDest(grunt, fileData, options);
+};
